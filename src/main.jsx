80be0d461e05566c90b756e1fcfc53cf067a8787
Olajide Michael <151611198+Fulfilled234@users.noreply.github.com>
2026-07-15 10:32:14 +0100

    Delete src/main.jsx

--- src/main.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import App from "./EduNexus"
-
-ReactDOM.createRoot(document.getElementById("root")).render(<App/>)
-                    
